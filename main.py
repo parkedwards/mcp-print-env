451a11ead0a134023655770b4b633f48fc9f99ed
Edward Park <ed.sh.park@gmail.com>
2025-07-29 10:38:48 -0700

    add requests to lambda runtime api

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastmcp import FastMCP
 import os
+import requests
 
-mcp: FastMCP = FastMCP("print-env")
+mcp: FastMCP = FastMCP("print-env", "runtime-next")
 
 @mcp.tool()
 def print_env() -> dict[str, str]:
     return dict(os.environ)
+
+@mcp.tool()
+def runtime_next():
+    runtime_api = os.environ.get("AWS_LAMBDA_RUNTIME_API")
+    if not runtime_api:
+        raise Exception("AWS_LAMBDA_RUNTIME_API is not set")
+    response = requests.post(f"http://{runtime_api}/2020-01-01/runtime/invocation/next")
+    return response.json()
 
 if __name__ == "__main__":
     mcp.run(
